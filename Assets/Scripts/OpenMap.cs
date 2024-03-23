3da0cc886d51d41ff16e872635501ae5f6b77727
odsajomale <oluwadara2.ajomale@live.uwe.ac.uk>
2024-03-23 16:26:42 +0000

    added map + map texture

--- Assets/Scripts/OpenMap.cs
+++ Assets/Scripts/OpenMap.cs
@@ -3,16 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OpenMap : MonoBehaviour
+
+
 {
+    public GameObject ui;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       HideUI();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ShowUI();
+
+
+        }
+    }
+
+    public void HideUI ()
+    {
+        ui.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void ShowUI()
+    {
+        ui.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void CloseMap()
+    {
+     HideUI();
+
     }
 }
